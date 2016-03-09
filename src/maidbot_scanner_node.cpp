21dcae3a9c0114415f88d340d234e9b5691bffbd
Jackson Carter <cartejac@onid.oregonstate.edu>
2016-03-08 21:19:47 -0800

    Refactored parameter get methods and added noise to the sensor feed. Added simple readme with references for future work.

--- src/maidbot_scanner_node.cpp
+++ src/maidbot_scanner_node.cpp
@@ -10,42 +10,44 @@ using std::cout;
 using std::endl;
 using std::string;
 
-void get_node_parameters(double& scan_period)
+template <typename T>
+void get_param_or_exit(const char* param_name, T& p)
 {
-	if (!ros::param::get("laser_scan_period", scan_period)) {
-		cout << "Could not retrieve laser_scan_period from parameter server. Are you using the launch file> Terminating." << endl;
+	if (!ros::param::get(param_name, p)) {
+		cout << "Could not retrieve " << param_name << " from parameter server. Are you using the launch file? Terminating." << endl;
 		exit(1);
 	}
 
+}
+
+void get_node_parameters(double& scan_period)
+{
+	get_param_or_exit("laser_scan_period", scan_period);
 	if (scan_period < 0) {
 		cout << "Invalid scan period: negative time - " << scan_period << "\n. Terminating." << endl;
 		exit(1);
 	}
 }
 
-void create_template_scan(sensor_msgs::LaserScan& template_msg, double scan_period)
+/* Description: Many of the parameters of the laser scan are the same between publishes,
+ *	so this node saves time by creating a "template" message and filling in what
+ *	changes per publish. This function sets the shared values.
+ */
+void create_template_scan_msg(sensor_msgs::LaserScan& template_msg, double scan_period)
 {
 	/* Obtain message-specific parameters from the parameter server */
 	double scan_range = 0;
 	double angular_res = 0;
 	string scanner_frame("");
-	if (!ros::param::get("scanner_frame_id", scanner_frame)) {
-		cout << "Could not retrieve scanner_frame_id from parameter server. Are you using the launch file? Terminating." << endl;
-		exit(1);
-	}
-
-	if (!ros::param::get("scanner_angular_res", angular_res)) {
-		cout << "Could not retrieve scanner_angular_res from parameter server. Are you using the launch file? Terminating." << endl;
-		exit(1);
-	}
+	get_param_or_exit("scanner_frame_id", scanner_frame);
+	get_param_or_exit("scanner_angular_res", angular_res);
+	get_param_or_exit("scan_range", scan_range);
+	
+	/* Validate parameters */
 	if (angular_res < 0) {
 		cout << "Invalid (negative) angular resolution: " << angular_res << endl;
 	}
 
-	if (!ros::param::get("scan_range", scan_range)) {
-		cout << "Could not retrieve scan_range from parameter server. Are you using the launch file? Terminating." << endl;
-		exit(1);
-	}
 	if (scan_range < 0 || scan_range > (2 * M_PI)) {
 		cout << "Invalid scan_range: " << scan_range << endl;
 	}
@@ -64,35 +66,45 @@ void create_template_scan(sensor_msgs::LaserScan& template_msg, double scan_peri
 	template_msg.ranges.resize(n_samples);
 }
 
+/* Description: Sets the scan message's variable values
+ */
 void mk_scan(sensor_msgs::LaserScan& msg)
 {
 	msg.header.stamp = ros::Time::now();
 	
 	unsigned int n_samples = msg.ranges.size();
+	double noise = 0;	/* No sensor is perfect */
 	for (unsigned int i = 0; i < n_samples; i++) {
-		msg.ranges[i] = 3;
+		noise = 0.03 * ((rand() % 100) / 100.0);
+		msg.ranges[i] = 3 + noise;
 	}
 }
 
 int main(int argc, char** argv){
 	ros::init(argc, argv, "maidbot_scanner_node");
+	srand(0);
 	ros::NodeHandle nh;
 	double scan_period;
 	sensor_msgs::LaserScan msg;
 
-	/* Obtain node-specific parameters */
 	get_node_parameters(scan_period);
 
-	create_template_scan(msg, scan_period);
+	create_template_scan_msg(msg, scan_period);
 
 	ros::Publisher scan_pub = nh.advertise<sensor_msgs::LaserScan>("scan", 10);
-	ros::Duration scan_sleeper(scan_period);
+	ros::Duration scan_T(scan_period);
+	ros::Duration sleep_time;
+	ros::Time start_time;
 	while (ros::ok()) {
+		start_time = ros::Time::now();
 		mk_scan(msg);
 		cout << "Publishing laser scan." << endl;
 		scan_pub.publish(msg);
 
-		scan_sleeper.sleep();
+		/* Ensure the node publishes at a constant frequency, 
+		   regardless of computation */
+		sleep_time = scan_T - (ros::Time::now() - start_time);
+		sleep_time.sleep();
 	}
 
 	return EXIT_SUCCESS;
