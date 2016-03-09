e3030f262d953e150e8ff2d37f30ca6a8e81607b
Jackson Carter <cartejac@onid.oregonstate.edu>
2016-03-08 20:49:48 -0800

    Node is functional. Completed filling ranges and basic message information. Also updated frames of reference for the 'robot' so that the laser is oriented upwards.

--- src/maidbot_scanner_node.cpp
+++ src/maidbot_scanner_node.cpp
@@ -26,26 +26,52 @@ void get_node_parameters(double& scan_period)
 void create_template_scan(sensor_msgs::LaserScan& template_msg, double scan_period)
 {
 	/* Obtain message-specific parameters from the parameter server */
-	double scan_range = (3.0/4) * 3.14159; //TODO: Add this to the parameter server
+	double scan_range = 0;
+	double angular_res = 0;
 	string scanner_frame("");
 	if (!ros::param::get("scanner_frame_id", scanner_frame)) {
 		cout << "Could not retrieve scanner_frame_id from parameter server. Are you using the launch file? Terminating." << endl;
 		exit(1);
 	}
 
+	if (!ros::param::get("scanner_angular_res", angular_res)) {
+		cout << "Could not retrieve scanner_angular_res from parameter server. Are you using the launch file? Terminating." << endl;
+		exit(1);
+	}
+	if (angular_res < 0) {
+		cout << "Invalid (negative) angular resolution: " << angular_res << endl;
+	}
+
+	if (!ros::param::get("scan_range", scan_range)) {
+		cout << "Could not retrieve scan_range from parameter server. Are you using the launch file? Terminating." << endl;
+		exit(1);
+	}
+	if (scan_range < 0 || scan_range > (2 * M_PI)) {
+		cout << "Invalid scan_range: " << scan_range << endl;
+	}
+
+	/* Set the message's constants */
+	unsigned int n_samples = scan_range / angular_res;
 	template_msg.header.frame_id = scanner_frame;
-	template_msg.angle_min = ((2 * M_PI) - scan_range) / 2;
-	template_msg.angle_max = (2 * M_PI) - template_msg.angle_min;
-	template_msg.time_increment = scan_period; //TODO: Understand this parameter better
+	template_msg.angle_min = M_PI - (scan_range/2);
+	template_msg.angle_max = M_PI + (scan_range/2);
+	template_msg.angle_increment = angular_res;
+	template_msg.time_increment = scan_period / n_samples;
 	template_msg.scan_time = scan_period;
 	template_msg.range_min = 0.1;
 	template_msg.range_max = 30;
+	template_msg.intensities.clear();
+	template_msg.ranges.resize(n_samples);
 }
 
 void mk_scan(sensor_msgs::LaserScan& msg)
 {
-
 	msg.header.stamp = ros::Time::now();
+	
+	unsigned int n_samples = msg.ranges.size();
+	for (unsigned int i = 0; i < n_samples; i++) {
+		msg.ranges[i] = 3;
+	}
 }
 
 int main(int argc, char** argv){
@@ -56,7 +82,9 @@ int main(int argc, char** argv){
 
 	/* Obtain node-specific parameters */
 	get_node_parameters(scan_period);
-	
+
+	create_template_scan(msg, scan_period);
+
 	ros::Publisher scan_pub = nh.advertise<sensor_msgs::LaserScan>("scan", 10);
 	ros::Duration scan_sleeper(scan_period);
 	while (ros::ok()) {
